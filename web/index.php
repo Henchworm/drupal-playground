a75f8dc2c862b3b388950a5d76a9d505dd3a861f
Henchworm <agop5134@gmail.com>
2024-10-22 23:49:31 -0600

    updates index php for new drupal

--- web/index.php
+++ web/index.php
@@ -1,22 +1,9 @@
 <?php
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- */
-
-use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
-$autoloader = require_once 'autoload.php';
-
-$kernel = new DrupalKernel('prod', $autoloader);
-
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+$autoloader = require_once __DIR__ . '/autoload.php';
+$kernel = \Drupal\Core\DrupalKernel::createFromRequest(Request::createFromGlobals(), $autoloader, 'prod');
+$response = $kernel->handle(Request::createFromGlobals());
 $response->send();
-
-$kernel->terminate($request, $response);
+$kernel->terminate(Request::createFromGlobals(), $response);
